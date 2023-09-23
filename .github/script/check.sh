507fc428df830734c808b899a7768a4794e8071b
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2023-09-23 20:11:42 +0200

    ci(coding style): Add the ci for the coding style

---
+++ .github/script/check.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function my_readlink() {
+    cd $1
+    pwd
+    cd - > /dev/null
+}
+
+if [ $# = 2 ]; then
+    DELIVERY_DIR=$(my_readlink "$1")
+    REPORTS_DIR=$(my_readlink "$2")
+    EXPORT_FILE="$REPORTS_DIR"/coding-style-reports.log
+    SCRIPT=./.github/script/lambdananas
+    ### delete existing report file
+    rm -f "$EXPORT_FILE"
+
+    ### generate reports
+    $SCRIPT . > coding-style-reports.log
+fi
