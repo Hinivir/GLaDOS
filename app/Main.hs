502a5c1c425cacb9760f65d399fea773104aeb08
Pierre-Louis <pierre-louis.leroy@epitech.eu>
2023-09-29 16:15:14 +0200

    fix: Cyprien mistake with main documentation

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,18 @@ module Main (main) where
 import System.IO
 import System.Exit
 
+
+-- function errorExit
+-- Take a String
+-- Prints it on error output with exitWith 84 (for indicates an error)
 errorExit :: String -> IO ()
 errorExit msg = hPutStrLn stderr msg >> exitWith (ExitFailure 84)
 
+
+-- function mainCheckEOF
+-- Take a Boolean
+-- If true: then calls errorExit with "No input"
+-- If false: read standard input, display it, continue to the end of the file
 mainCheckEOF :: Bool -> IO ()
 mainCheckEOF True = errorExit "No input"
 mainCheckEOF False = do
@@ -21,5 +30,10 @@ mainCheckEOF False = do
                 isEOF' <- isEOF
                 mainCheckEOF isEOF'
 
+
+-- function main
+-- Take IO (it's the main)
+-- If no standard input then error
+-- Otherwise passes standard input to the rest of the program
 main :: IO ()
 main = isEOF >>= mainCheckEOF
