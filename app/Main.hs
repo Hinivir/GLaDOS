17d2cf85e080ff39038aac607ac9dd175cfb32fb
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2023-09-29 15:56:48 +0200

    fix: PL mistake

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,27 @@
 -}
 
 module Main (main) where
+import System.IO
+import System.Exit
 
-import Lib
 
+-- function errorExit
+-- Take a String
+-- Prints it on error output with exitWith 84 (for indicates an error)
+errorExit :: String -> IO ()
+errorExit msg = hPutStrLn stderr msg >> exitWith (ExitFailure 84)
+
+-- function mainCheckEOF
+-- Take a Boolean
+-- If true: then calls errorExit with "No input"
+-- If false: use getArgs to send input to chainedMap then mainLoop
+mainCheckEOF :: Bool -> IO ()
+mainCheckEOF True  = errorExit "No input"
+mainCheckEOF False = putStrLn "GLaDOS"
+
+-- function main
+-- Take IO (it's the main)
+-- If no standard input then error
+-- Otherwise passes standard input to the rest of the program
 main :: IO ()
-main = someFunc
+main = isEOF >>= mainCheckEOF
