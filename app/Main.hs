0e464afaed0e2d074c6b10b150629f8055c53e5f
Drindael <cyprien.nguyen-van-vien@epitech.eu>
2023-09-29 16:34:34 +0200

    feat: the end of the main of the parser

--- app/Main.hs
+++ app/Main.hs
@@ -10,16 +10,15 @@ module Main (main) where
 import System.IO
 import System.Exit
 
+
 errorExit :: String -> IO ()
 errorExit msg = hPutStrLn stderr msg >> exitWith (ExitFailure 84)
 
-mainCheckEOF :: Bool -> IO ()
-mainCheckEOF True = errorExit "No input"
-mainCheckEOF False = do
-                line <- getLine
-                putStrLn line
-                isEOF' <- isEOF
-                mainCheckEOF isEOF'
+mainCheckFile :: IO ()
+mainCheckFile = do
+    contents <- getContents
+    putStr contents
 
 main :: IO ()
-main = isEOF >>= mainCheckEOF
+main = mainCheckFile
+
