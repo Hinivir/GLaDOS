2c0d11929b7694fb652b835dabc166d68a5cf3a7
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2023-10-02 10:39:49 +0200

    fix: upgrade main.hs to be more efficient and delete some useless comments in Ast.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,41 @@
+{- 
+-- EPITECH PROJECT, 2023
+-- glados_mirror
+-- File description:
+-- Main.hs
+-}
+
 module Main (main) where
 
 import System.IO
 import System.Exit
 
--- Print an error message to stderr and exit with code 84
+-- function errorExit
+-- Take a String
+-- Prints it on error output with exitWith 84 (for indicates an error)
 errorExit :: String -> IO ()
 errorExit msg = hPutStrLn stderr msg >> exitWith (ExitFailure 84)
 
--- Check for EOF and process standard input
+-- function mainCheckEOF
+-- Take a Boolean
+-- If true: then calls errorExit with "No input"
+-- If false: read standard input, display it, continue to the end of the file
 mainCheckEOF :: IO ()
 mainCheckEOF = do
     line <- getLine
     putStrLn line
     isEOF' <- isEOF
-    if isEOF' then return () else mainCheckEOF
+    case isEOF' of
+        True -> return ()
+        False -> mainCheckEOF
 
+-- function main
+-- Take IO (it's the main)
+-- If no standard input then error
+-- Otherwise passes standard input to the rest of the program
 main :: IO ()
 main = do
     isEOF' <- isEOF
-    if isEOF' then errorExit "No input" else mainCheckEOF
+    case isEOF' of
+        True -> errorExit "No input"
+        False -> mainCheckEOF
