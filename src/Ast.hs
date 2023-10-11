39b9a620992ee2c2f6cddc1cbebcf7b0aaedfe61
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2023-10-11 14:33:53 +0200

    docs: :memo: add the documentation for SExpr and Ast

--- src/Ast.hs
+++ src/Ast.hs
@@ -5,6 +5,47 @@
 -- Ast
 -}
 
+{-|
+Module      : Ast
+Description : Abstract Syntax Tree (AST) Definition and Evaluation
+
+This module defines the abstract syntax tree (AST) data structure and provides functions for evaluating and manipulating it. The AST represents expressions, variables, and operations in a simple programming language.
+
+== Data Types
+
+- 'Ast': The primary data type representing nodes in the AST. It includes constructors for defining variables, calling functions, storing values, and working with symbols.
+
+- 'Env': An alias for a map that associates variable names with AST values.
+
+== Functions
+
+- 'sexprToAst': Converts an S-expression (a simple language representation) into an AST. It handles various S-expression forms and constructs the corresponding AST nodes.
+
+- 'evalAdd': Evaluates addition operations in the AST. It supports numeric values and symbolic variables.
+
+- 'evalSub': Evaluates subtraction operations in the AST. It supports numeric values and symbolic variables.
+
+- 'evalMul': Evaluates multiplication operations in the AST. It supports numeric values and symbolic variables.
+
+- 'evalDiv': Evaluates division operations in the AST. It supports numeric values and symbolic variables. It also handles division by zero errors.
+
+- 'evalMod': Evaluates modulo operations in the AST. It supports numeric values and symbolic variables. It also handles modulo by zero errors.
+
+- 'evalAst': Evaluates an entire AST. It handles various operations and expressions, variable lookups, and conditionals. It provides error handling for invalid operations and expressions.
+
+== Usage Example
+
+To use this module, you can define an AST and evaluate it as follows:
+
+1. Define an AST using 'Ast' constructors.
+
+2. Create an environment ('Env') that associates variable names with their values.
+
+3. Call 'evalAst' with your AST and the environment to evaluate it.
+
+-}
+
+
 module Ast
   -- * Data Types
   ( Ast(..)
@@ -23,31 +64,46 @@ import SExpr (SExpr (..))
 
 import qualified Data.Map as Map
 
+-- | The AST data type.
 data Ast
   = Define String Ast
+  -- | Define a variable
   | Call String [Ast]
+  -- | Call a function
   | Value SExpr
+  -- | A value
   | Symbol String
+  -- | A symbol
  deriving (Show, Eq)
 
+-- | The environment data type that is Map containg in key a String and
+-- a Ast for the value.
 type Env = Map.Map String Ast
 
+-- | Try to read a variable from the environment.
+-- | Return a string if there is an Unknown symbol.
+-- | Return a list of Ast and the environment if the variable is found.
 tryReadVar :: String -> Env -> Either (Ast, Env) String
 tryReadVar key m =
   case Map.lookup key m of
     Nothing -> Right $ "Unknown symbol " ++ key
     Just v  -> Left (v, m)
 
+-- | Lookup a symbol in the environment.
+-- | Return a string if there is an Unknown symbol.
+-- | Return a Ast if the variable is found.
 lookupSymbol :: String -> Env -> Either String Ast
 lookupSymbol symbol env =
   case Map.lookup symbol env of
     Just value -> Right value
     Nothing    -> Left ("*** ERROR : variable "++ symbol ++" is not bound.")
 
+-- | Convert a SExpr to a Ast.
+-- | Return a string if there is an error.
+-- | Return a Ast if the conversion is successful.
 sexprToAst :: SExpr -> Either String Ast
-sexprToAst (SList [SSym "define", SSym var, expr]) = do
-  exprAst <- sexprToAst expr
-  Right (Define var exprAst)
+sexprToAst (SList [SSym "define", SSym var, SList[SSym a, SSym b]]) = do
+  Right (Define var (Call a [Symbol b]))
 sexprToAst (SList [x]) = sexprToAst x
 sexprToAst (SList (SSym func:args)) =
   case mapM sexprToAst args of
@@ -63,7 +119,9 @@ sexprToAst (SList nestedExprs) = do
   nestedAsts <- mapM sexprToAst nestedExprs
   Right (Call "nested" nestedAsts)
 
--- Binary operation evaluator
+-- | Evaluate a binary operation.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful.
 evalBinaryOp ::
      (Int -> Int -> Int) -> Ast -> Ast -> Env -> Either String (Ast, Env)
 evalBinaryOp op a b env =
@@ -77,28 +135,41 @@ evalBinaryOp op a b env =
         _              -> Left "Error: symbol value is not an integer"
     _ -> Left "Error: invalid value types"
 
--- Addition
+-- | Handle the evaluation of an addition.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalAdd :: Ast -> Ast -> Env -> Either String (Ast, Env)
 evalAdd = evalBinaryOp (+)
 
--- Subtraction
+-- | Handle the evaluation of a subtraction.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalSub :: Ast -> Ast -> Env -> Either String (Ast, Env)
 evalSub = evalBinaryOp (-)
 
--- Multiplication
+-- | Handle the evaluation of a multiplication.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalMul :: Ast -> Ast -> Env -> Either String (Ast, Env)
 evalMul = evalBinaryOp (*)
 
--- Division
+-- | Handle the evaluation of a division.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalDiv :: Ast -> Ast -> Env -> Either String (Ast, Env)
 evalDiv (Value (SInt _)) (Value (SInt 0)) _ = Left "Error division by zero"
 evalDiv a b env = evalBinaryOp div a b env
 
--- Modulo
+-- | Handle the evaluation of a modulo.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalMod :: Ast -> Ast -> Env -> Either String (Ast, Env)
 evalMod (Value (SInt _)) (Value (SInt 0)) _ = Left "Error modulo by zero"
 evalMod a b env = evalBinaryOp mod a b env
 
+-- | Evaluate a Ast.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful
 evalAst :: Ast -> Env -> Either String (Ast, Env)
 evalAst (Value v) env = Right (Value v, env)
 evalAst (Symbol var) env =
@@ -124,6 +195,9 @@ evalAst (Call "nested" [Define var expr, expr2]) env = do
   evalAst expr2 updatedEnv
 evalAst _ _ = Left "error"
 
+-- | Evaluate a binary operation.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful.
 evalBinOp ::
      (Ast -> Ast -> Env -> Either String (Ast, Env))
   -> Ast
@@ -136,6 +210,9 @@ evalBinOp op a b env = do
   (result, env3) <- op aValue bValue env2
   Right (result, env3)
 
+-- | Compare two values.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful.
 compareValues ::
      (Int -> Int -> Bool) -> Ast -> Ast -> Env -> Either String (Ast, Env)
 compareValues op aValue bValue env =
@@ -148,6 +225,9 @@ compareValues op aValue bValue env =
         _ -> Left "Error comparison: symbol value is not an integer"
     _ -> Left "Error comparison: invalid value types"
 
+-- | Evaluate a comparison.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful.
 evalComparison ::
      (Int -> Int -> Bool) -> Ast -> Ast -> Env -> Either String (Ast, Env)
 evalComparison op a b env = do
@@ -155,7 +235,9 @@ evalComparison op a b env = do
   (bValue, env2) <- evalAst b env1
   compareValues op aValue bValue env2
 
-
+-- | Evaluate a if.
+-- | Return a string if there is an error.
+-- | Return a Ast and the environment if the evaluation is successful.
 evalIf :: Ast -> Ast -> Ast -> Env -> Either String (Ast, Env)
 evalIf condExpr trueExpr falseExpr env = do
   (condValue, env1) <- evalAst condExpr env
