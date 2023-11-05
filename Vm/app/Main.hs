0f90f2074f6e79ce0d1ac04754245a1619288d81
Drindael <cyprien.nguyen-van-vien@epitech.eu>
2023-11-05 11:52:28 +0100

    fix: removing warning in main for bytescode

--- Vm/app/Main.hs
+++ Vm/app/Main.hs
@@ -9,19 +9,22 @@ module Main
   ( main
   ) where
 
-import System.Exit
-import System.IO
+import System.Exit ()
+import System.IO ()
 
 import Vm (Instructions, Env, exec)
 
 main :: IO ()
 main = do
     fileContents <- readFile "lip.lop"
-    let (instructionsLine:envLine:_) = lines fileContents
-    let instructions = read instructionsLine :: Instructions
-    let env = read envLine :: Env
-    let result = exec [] env instructions []
+    let linesList = lines fileContents
+    case linesList of
+        (instructionsLine:envLine:_) -> do
+            let instructions = read instructionsLine :: Instructions
+            let env = read envLine :: Env
+            let result = exec [] env instructions []
+            case result of
+                Right val -> print val
+                Left err -> putStrLn $ "Error: " ++ err
+        _ -> putStrLn "Error: The file does not contain enough lines"
 
-    case result of
-        Right val -> print val
-        Left err -> putStrLn $ "Error: " ++ err
