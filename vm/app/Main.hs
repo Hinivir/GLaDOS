73b7c3b60e4f1841e5690d8cb9f60fe74355380c
BENAIS--CAPTAL <damien.benais-captal@epitech.eu>
2023-11-05 15:52:59 +0100

    fix: correct print return

--- vm/app/Main.hs
+++ vm/app/Main.hs
@@ -12,7 +12,12 @@ module Main
 import System.Exit ()
 import System.IO ()
 
-import Vm (Instructions, Env, exec)
+import Vm (
+    Instructions,
+    Env,
+    exec,
+    Value(..),
+    )
 
 main :: IO ()
 main = do
@@ -28,7 +33,18 @@ processFileContents fileContents =
             in executeInstructions env instructions
         _ -> putStrLn "Error: The file does not contain enough lines"
 
+printValue :: Value -> IO ()
+printValue (Number n) = print n
+printValue (Float f) = print f
+printValue (Boolean b) = print b
+printValue (Char c) = print c
+printValue (Op op) = print op
+printValue (Builtin builtin) = print builtin
+printValue (List list) = print list
+printValue (String string) = print string
+printValue _ = putStrLn "Error: Cannot print this value"
+
 executeInstructions :: Env -> Instructions -> IO ()
 executeInstructions env instructions = case exec [] env instructions [] of
-    Right val -> print val
+    Right val -> printValue val
     Left err -> putStrLn $ "Error: " ++ err
